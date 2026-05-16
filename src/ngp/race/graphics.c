d435b9094af6d4cccf142673c42a9c81254f3536
mariovirgili <giorgiopiras@gmail.com>
2026-05-16 15:51:56 +0200

    NGP: Fix Neo Geo Pocket monochrome palette conversion and Improve Neo Geo Pocket monochrome K1GE rendering.

--- src/ngp/race/graphics.c
+++ src/ngp/race/graphics.c
@@ -611,13 +611,15 @@ void myGraphicsBlitLine(unsigned char render)
             // couleurs OOW + fond
             const uint8_t  oow_idx = (uint8_t)(*oowSelect & 0x07);
             const uint16_t OOWCol  = oowTable ? NGPC_TO_SDL16(oowTable[oow_idx]) : 0;
+            const uint8_t frame1 = *frame1Pri;
+            const bool plane2_front = is_bw ? ((frame1 & 0x01) != 0) : ((frame1 & 0x80) != 0);
 
             uint16_t bgcol;
             const uint8_t bgsel = *bgSelect;
-            if (bgsel & 0x80) {
+            if (is_bw) {
+                bgcol = OOWCol;
+            } else if (bgsel & 0x80) {
                 bgcol = NGPC_TO_SDL16(bgTable[bgsel & 0x07]);
-            } else if (is_bw) {
-                bgcol = NGPC_TO_SDL16(bwTable[0]);
             } else {
                 bgcol = palette_table ? NGPC_TO_SDL16(palette_table[0]) : 0;
             }
@@ -645,7 +647,7 @@ void myGraphicsBlitLine(unsigned char render)
                         myPalettes[64 + i]  = NGPC_TO_SDL16(bwTable[bw_palette_table[8  + i] & 0x07]);
                         myPalettes[68 + i]  = NGPC_TO_SDL16(bwTable[bw_palette_table[12 + i] & 0x07]);
                         myPalettes[128 + i] = NGPC_TO_SDL16(bwTable[bw_palette_table[16 + i] & 0x07]);
-                        myPalettes[132 + i] = NGPC_TO_SDL16(bw_palette_table[20 + i] & 0x07);
+                        myPalettes[132 + i] = NGPC_TO_SDL16(bwTable[bw_palette_table[20 + i] & 0x07]);
                     }
                 } else if (palette_table) {
                     // 192 entries NGPC
@@ -661,11 +663,10 @@ void myGraphicsBlitLine(unsigned char render)
                 drawSprites(draw, mySprPri40->refs, mySprPri40->count, x0, x1);
 
                 // plans + 0x80 entre les deux
-                const uint8_t frame1 = *frame1Pri;
                 const uint8_t sfX = *scrollFrontX, sfY = (uint8_t)(*scrollFrontY + y);
                 const uint8_t sbX = *scrollBackX,  sbY = (uint8_t)(*scrollBackY  + y);
 
-                if (frame1 & 0x80) {
+                if (plane2_front) {
                     // FRONT, SPRITES 0x80, BACK
                     drawScrollPlane(draw, tile_table_front,  64,  sfX, sfY, x0, x1, is_bw);
                     drawSprites(draw, mySprPri80->refs, mySprPri80->count, x0, x1);
@@ -684,9 +685,9 @@ void myGraphicsBlitLine(unsigned char render)
 #ifdef NGP_TRACE_LOGS
             if (s_ngp_trace_gfx_frame < 5 || ((s_ngp_trace_gfx_frame % 60u) == 0u)) {
                 if (y == 0u || y == 76u || y == 151u) {
-                    printf("[NGP][LINE f=%u y=%u] render=%u bw=%d winEmpty=%d x=%d..%d bg=%04X oow=%04X pri=%u/%u/%u pix=%04X/%04X/%04X pal=%04X/%04X tileF=%04X tileB=%04X scrollF=%u,%u scrollB=%u,%u\n",
+                    printf("[NGP][LINE f=%u y=%u] render=%u bw=%d winEmpty=%d x=%d..%d bg=%04X oow=%04X f1=%02X p2front=%u pri=%u/%u/%u pix=%04X/%04X/%04X pal=%04X/%04X tileF=%04X tileB=%04X scrollF=%u,%u scrollB=%u,%u\n",
                            s_ngp_trace_gfx_frame, y, (unsigned)render, is_bw, win_empty,
-                           x0, x1, bgcol, OOWCol,
+                           x0, x1, bgcol, OOWCol, frame1, (unsigned)plane2_front,
                            mySprPri40 ? mySprPri40->count : 0,
                            mySprPri80 ? mySprPri80->count : 0,
                            mySprPriC0 ? mySprPriC0->count : 0,
